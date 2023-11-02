4bfeac702218f18942ef81faab89746b655e036f
YuSetiawan <ysfstiawan@gmail.com>
2023-11-02 13:42:34 +0700

    Style props

---
+++ src/pages/StyleProps.tsx
@@ -0,0 +1,9 @@
+import {Container} from '../components/Container';
+
+export const StyleProps = () => {
+  return (
+    <div>
+      <Container styles={{border: '2px solid black', padding: '1rem'}} />
+    </div>
+  );
+};
